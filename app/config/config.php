cc76a6e9983cd1ff79387661ba6074a11077b3ff
AlexandrosMelissas <melissasalexandros@gmail.com>
2020-06-05 20:46:07 +0200

    Init

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,28 @@
 <?php
 
-    // DB Config
-    define('DBHOST','localhost');
-    define('DBNAME', 'new_project');
-    define('DBUSER', 'root');
-    define('DBPASS','');
-
-    // App Root
-    define('APPROOT',dirname(dirname(__FILE__)));
-
-    // Public ROOT
-    define('URLROOT','http://localhost/php');
+ if($_SERVER['HTTP_HOST'] == 'localhost'){
+      // DB Config
+      define('DBHOST','localhost');
+      define('DBNAME', 'new_project');
+      define('DBUSER', 'root');
+      define('DBPASS','');
+  
+      // App Root
+      define('APPROOT',dirname(dirname(__FILE__)));
+  
+      // Public ROOT
+      define('URLROOT','http://localhost/php');
+ } else {
+       // DB Config
+       define('DBHOST','localhost');
+       define('DBNAME', 'new_project');
+       define('DBUSER', 'root');
+       define('DBPASS','');
+   
+       // App Root
+       define('APPROOT',dirname(dirname(__FILE__)));
+   
+       // Public ROOT
+       define('URLROOT','https://powerful-tor-98459.herokuapp.com');
+ }
+  
