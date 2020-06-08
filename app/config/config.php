b2c6ccf8bc880082f810b5ad87738f504ace07c7
AlexandrosMelissas <melissasalexandros@gmail.com>
2020-06-08 17:08:42 +0200

    Final Changes

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+       // DB Config
+       define('DBHOST','localhost');
+       define('DBNAME', 'blogposts');
+       define('DBUSER', 'root');
+       define('DBPASS','');
+   
+       // App Root
+       define('APPROOT',dirname(dirname(__FILE__)));
+   
+       // Public ROOT
+       define('URLROOT','http://users.iee.ihu.gr/~it154582/BlogPosts');
+
+  
